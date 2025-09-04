6db0e79aad9ce51b04871cf3c8488fff72b81c5c
luistoledolafuente <luismiguel.tlf@gmail.com>
2025-09-04 17:14:00 -0500

    create destroy...

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -2,4 +2,10 @@ package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
 
+    public void destroy(int id) {
+        // Lógica para eliminar un estudiante por su ID
+        System.out.println("Eliminando estudiante con ID: " + id);
+        // Aquí iría la llamada al servicio o repositorio que elimina el estudiante
+    }
+
 }
